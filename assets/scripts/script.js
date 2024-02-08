043544f2ae73aabae41d3a6b9a12301ba6ffa368
phpiiper <plp@gmail.com>
2024-02-08 13:26:43 -0500

    Minor Fixes: - Removed dev function from automatically executing

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -7,4 +7,4 @@ pd("homepage-options").childNodes[0].click()
 createBlueprintDiv("content",storyList[0])
 pd("blueprintDiv").preview()
 
-pd(".bpItemDiv")[2].childNodes[1].click()
+// pd(".bpItemDiv")[2].childNodes[1].click()
